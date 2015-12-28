489eda803ac8ebb320d025a28b9aec3a63b64437
Artix <julespondard@gmail.com>
2015-12-28 17:33:58 +0100

    Avancement 2, ajout des strings sur le segment de données, compilation des decl presque ok (il faut gérer l'alloc mémoire correctement), et expressions en cours

--- template/compile.ml
+++ template/compile.ml
@@ -3,7 +3,9 @@ open Ast
 open Format
 open Typage
 
-let (genv : (string, unit) Hashtbl.t) = Hashtbl.create 17
+let (genv : (string, unit) Hashtbl.t) = Hashtbl.create 42
+
+let liste_str = ref []
 
 module Smap = Map.Make(String)
 
@@ -42,35 +44,63 @@ let compileConstruct ident plnames debutTas =
 	let res = 
 		label ("C_"^ident) ++
         (fst (List.fold_left (fun (c,decal) x -> (movq (ind ~ofs:(debutPile+decal) rbp) (ind ~ofs:(debutTas+decal) rsp)) ++ c, decal+8) (nop, 0) plnames)
-		) ++
-		ret
+		)
 	in
 	res
 
 let getPlNames pl =
 	List.map fst pl
 
-let compile_expr exp env = 
-	nop
+let getCstVal cst = match cst with
+  | Cunit -> imm 0
+  | Cint(res) -> imm res (*sur 64 bits, il va falloir remplacer tous les 8 par des 64 *)
+  | Cbool(b) -> imm (if b then 1 else 0)
+  | Cstring(str) -> liste_str := str::(!liste_str); ilab ("str_"^(string_of_int (List.length (!liste_str))))
 
-let rec compileDecl_l classe pdecl_l newFun ordreVar = 
-	assert(false);
+let compile_expr exp env = 
+	match fst exp with
+	| Ecst(cst) -> pushq (getCstVal cst)
+    (*| Ethis
+    | Enull
+    | Eaccess of left_value
+    | Eaffect of left_value * locd_expr * (pos)
+    | Ecall of left_value * args_type * (locd_expr list)
+    | Enew  of nom_classe * args_type * (locd_expr list)
+    | Eunop of unop * locd_expr
+    | Ebinop of binop * locd_expr * locd_expr * (pos)
+    | Eif of locd_expr * locd_expr * locd_expr
+    | Ewhile of locd_expr * locd_expr
+    | Ereturn of locd_expr
+    | Eprint of locd_expr
+    | Ebloc of instruction list*)
+    | _ -> nop
+
+let rec compileDecl_l classe pdecl_l newFun ordreVar debutConstruct = 
 	match pdecl_l with
-	| [] -> newFun, ordreVar
-	| a::b -> compileDecl classe a b newFun ordreVar
-and compileDecl classe decl reste newFun ordreVar = 
+	| [] -> newFun, ordreVar,debutConstruct
+	| a::b -> compileDecl classe a b newFun ordreVar debutConstruct
+and compileDecl classe decl reste newFun ordreVar debutConstruct = 
 	match decl with
-    | Dvar(var) -> let (_, ident, _, exp, _) = var in
+    | Dvar(var) -> let (_, ident, _, expr, _) = var in
     				let ordreVar = Smap.add classe (ident::(Smap.find classe ordreVar)) ordreVar in
     				(* TODO : allouer de la place pour l'expression *)
-    				compile_expr exp (Smap.empty); (*dans quoi mettre ça ?*)
+    				let ce = compile_expr expr (Smap.empty) in (*dans quoi mettre ça ?*) (*le res est en haut de la pile, mettons le dans rbx*)
+    				let debutConstruct = debutConstruct ++ ce ++ popq rbx ++ (movq (reg rbx) (ind ~ofs:(8*(List.length (Smap.find classe ordreVar))) rsp)) in
     				(*TODO : libérer la place *)
-    				compileDecl_l classe reste newFun ordreVar
+    				compileDecl_l classe reste newFun ordreVar debutConstruct
     | Dmeth(methode) -> let (_,ident,ptl, pl, _ , expr, _) = methode in
     					let env,decal = List.fold_left (fun (ev,nxt) x -> Smap.add x nxt ev, nxt+8) (Smap.empty, 16) (getPlNames pl) in
-    					let cexp = compile_expr exp env in
-    					(* TODO compiler la méthode ici *)
-    					compileDecl_l classe reste newFun ordreVar
+    					let fpmax = 42 in (* TODO *)
+                        let ce = compile_expr expr env in
+    					let code = 
+    						label ("M_"^classe^"_"^ident) ++
+    					    pushq (reg rbp) ++
+      						movq (reg rsp) (reg rbp) ++ pushn fpmax ++
+      						ce ++ popq rax ++
+      						popn fpmax ++ popq rbp ++ ret
+      					in
+      					let newFun = newFun ++ code in
+    					compileDecl_l classe reste newFun ordreVar debutConstruct
 	
 let compile_class (codefun, codedesc, mMeth, ordreMeth, ordreVar) (Class(ident,_,ptcl,pl,(typ_pere, lexprl),pdecl_l)) = 
 	let (idPere,_,_)=typ_pere in
@@ -84,9 +114,9 @@ let compile_class (codefun, codedesc, mMeth, ordreMeth, ordreVar) (Class(ident,_
 	let plnames = (getPlNames pl) in
 	let ordreVar = Smap.add ident (lpere @ plnames) ordreVar in
 	(*Attention, les vars d'une classe sont allouées sur le tas*)
-	let newFun = (compileConstruct ident plnames (8+(List.length lpere)*8)) in
-	let newFun,ordreVar = compileDecl_l ident pdecl_l newFun ordreVar in
-	let newFun = newFun ++ codefun in
+	let debutConstruct = (compileConstruct ident plnames (8+(List.length lpere)*8)) in
+    let newFun,ordreVar,constructFini = compileDecl_l ident pdecl_l codefun ordreVar debutConstruct in
+	let newFun = constructFini ++ ret ++ newFun ++ codefun in
 
 	newFun, newDesc, mMeth, newOrdreMeth, ordreVar
 
@@ -135,8 +165,8 @@ let compile_program p ofile mMeth =
         codefun;
         
       data =
-        (Hashtbl.fold (fun x _ l -> label x ++ dquad [1] ++ l) genv
-          (label ".Sprint_int" ++ string "%d\n" ++ label ".Sprint_string" ++ string "%s\n"))
+        fst (List.fold_left (fun (code,nb) x -> label ("str_"^(string_of_int nb)) ++ string x ++ code, nb-1)
+        ((label ".Sprint_int" ++ string "%d\n" ++ label ".Sprint_string" ++ string "%s\n"),(List.length (!liste_str))) (!liste_str))
           ++ codedesc
     }
   in
